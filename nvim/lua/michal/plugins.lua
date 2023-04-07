798bcebc977bb79b18a93d33e7fba8279c0cf0fe
Michal Janik <michal@tacky.horse>
2023-04-07 16:05:46 +0200

    Use lualine instead of heirline

--- nvim/lua/michal/plugins.lua
+++ nvim/lua/michal/plugins.lua
@@ -72,7 +72,40 @@ return packer.startup(function(use)
 	use("nvim-treesitter/playground")
 
 	use("nvim-tree/nvim-web-devicons")
-	use("rebelot/heirline.nvim")
+	use({
+		"nvim-lualine/lualine.nvim",
+		config = function()
+			require("lualine").setup({
+				options = {
+					theme = bubbles_theme,
+					component_separators = "|",
+					section_separators = { left = "", right = "" },
+				},
+				sections = {
+					lualine_a = {
+						{ "mode", separator = { left = "" }, right_padding = 2 },
+					},
+					lualine_b = { "filename", "branch" },
+					lualine_c = { "fileformat" },
+					lualine_x = {},
+					lualine_y = { "filetype", "progress" },
+					lualine_z = {
+						{ "location", separator = { right = "" }, left_padding = 2 },
+					},
+				},
+				inactive_sections = {
+					lualine_a = { "filename" },
+					lualine_b = {},
+					lualine_c = {},
+					lualine_x = {},
+					lualine_y = {},
+					lualine_z = { "location" },
+				},
+				tabline = {},
+				extensions = {},
+			})
+		end,
+	})
 
 	use("akinsho/toggleterm.nvim")
 	use("folke/lsp-colors.nvim")

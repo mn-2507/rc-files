61b76edc1b7b4f0219812201879d5cb3cafab8be
Michal Janik <michal@tacky.horse>
2023-04-08 14:45:56 +0200

    Add a keymap for revealing the current file in the tree

--- nvim/lua/michal/neo-tree.lua
+++ nvim/lua/michal/neo-tree.lua
@@ -1,5 +1,8 @@
-require("neo-tree").setup {
-	vim.keymap.set("n", "<leader>tt", "<cmd>Neotree toggle<cr>",
-		{silent = true, noremap = true}
-	)
-}
+require("neo-tree").setup()
+
+vim.keymap.set("n", "<leader>tt", "<cmd>Neotree toggle<cr>",
+	{silent = true, noremap = true}
+)
+vim.keymap.set("n", "<c-n>", "<cmd>NeoTreeReveal<cr>",
+	{silent = true, noremap = true}
+)

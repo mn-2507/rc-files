51f98d01ae6d2f1aedc1d761afe846fcf7fdb38f
Michal Janik <michal@tacky.horse>
2023-04-08 14:49:36 +0200

    Add cokeline

--- nvim/lua/michal/plugins.lua
+++ nvim/lua/michal/plugins.lua
@@ -152,6 +152,13 @@ return packer.startup(function(use)
 			})
 		end,
 	})
+
+	use({
+		"willothy/nvim-cokeline",
+		config = function()
+			require("cokeline").setup()
+		end
+	})
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then

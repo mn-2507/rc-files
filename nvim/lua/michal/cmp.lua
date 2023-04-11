c7f00f77231fc379da3c265e211fb114b83632f1
Michal Janik <michal@tacky.horse>
2023-04-11 12:51:31 +0200

    Fix cmp init

--- nvim/lua/michal/cmp.lua
+++ nvim/lua/michal/cmp.lua
@@ -1,6 +1,12 @@
 local cmp = require("cmp")
 
 cmp.setup({
+	snippet = {
+		-- REQUIRED - you must specify a snippet engine
+		expand = function(args)
+			require('luasnip').lsp_expand(args.body)
+		end,
+	},
 	mapping = cmp.mapping.preset.insert({
 		["<C-b>"] = cmp.mapping.scroll_docs(-4),
 		["<C-f>"] = cmp.mapping.scroll_docs(4),

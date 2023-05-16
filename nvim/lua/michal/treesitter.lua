de1a568f1b03a50d4cff9ababf0b8b2d461d42f6
Michal Janik <michal@tacky.horse>
2023-05-16 08:53:55 +0200

    Ensure TS installed base langs

--- nvim/lua/michal/treesitter.lua
+++ nvim/lua/michal/treesitter.lua
@@ -1,5 +1,6 @@
 local configs = require("nvim-treesitter.configs")
 configs.setup {
+	ensure_installed = {'lua', 'python', 'vim'},
   sync_install = false,
 	auto_install = true,
   ignore_install = { "" }, -- List of parsers to ignore installing
